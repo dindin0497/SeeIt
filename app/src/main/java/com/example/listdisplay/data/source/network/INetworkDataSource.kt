7b160de2e240283a3296c582391213851ea92455
dindin <dindin0497@gmail.com>
2025-08-10 13:50:20 -0700

    1

--- app/src/main/java/com/example/listdisplay/data/source/network/INetworkDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.listdisplay.data.source.network
-
-import com.example.listdisplay.data.model.AnimResponse
-import retrofit2.Response
-
-interface INetworkDataSource {
-    suspend fun get(query: String): AnimResponse
-}
